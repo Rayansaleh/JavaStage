4c5aa70bbbc7f09d5f1dd7c230a5d738b776bf07
Rayansaleh <rayan@saleh.fr>
2017-09-27 17:02:05 +0200

    effacer

--- Animation/src/com/company/MoyenneEcartTyp/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company.MoyenneEcartTyp;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        Fenetre fen = new Fenetre();
-    }
-}
--- War Z/src/com/company/MoyenneEcartTyp/Operation.java
+++
@@ -1,8 +0,0 @@
-package com.company.MoyenneEcartTyp;
-
-public class Operation implements Soin{
-    @Override
-    public void soigne() {
-
-    }
-}
